79f8192ff87e2e69272a97cec20f9f23ab93c794
Josias <josiasguerrero2004@gmail.com>
2025-09-30 12:29:07 -0600

    first commit base

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ListaTareas from './componentes/ListaTareas/ListaTareas'
+import Home from './views/Home'
+
+function App() {
+  return (
+    <>
+      <Home>
+        <ListaTareas />
+      </Home>
+    </>
+  )
+}
+
+export default App
---
+++ src/views/Home.tsx
@@ -0,0 +1,23 @@
+import { FC, ReactNode } from 'react'
+import './Home.css'
+import Navbar from '../componentes/Navbar/Navbar'
+
+interface IHomeProps {
+  children: ReactNode
+  titulo?: string
+}
+
+const Home: FC<IHomeProps> = ({ children, titulo = 'AluraTask' }) => {
+  return (
+    <>
+      <Navbar titulo={titulo} />
+      <section className='main'>
+        <div className='container'>
+          <div id='task-list'>{children}</div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Home
